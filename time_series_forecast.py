9e1a4bb51741a9ec1c617b0262e668c66863ce86
Jackgil123 <1jackgilchrist@gmail.com>
2025-12-29 11:16:13 +0000

    correct main

--- time_series_forecast.py
+++ time_series_forecast.py
@@ -8,3 +8,10 @@ from statsmodels.tsa.arima.model import ARIMA
 data = pd.read_csv("C:\\Users\\1jack\\CyberjackyG\\datasets\\csvs\\monthly-beer-production-in-austr.csv", index_col='Month', parse_dates=True)
 plot_acf(data['Monthly beer production'], lags=30)
 plt.show()
+
+plot_pacf(data['Monthly beer production'], lags=30)
+plt.show()
+
+model = ARIMA(data['Monthly beer production'], order=(2, 1, 2))
+model_fit = model.fit()
+print(model_fit.summary())
